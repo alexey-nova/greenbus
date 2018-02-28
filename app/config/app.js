a57ae8104812ed8925f900af6a7f150d19d17a69
Almat Ybray <almat.ybray@gmail.com>
2018-03-01 00:25:15 +0600

    app config

--- app/config/app.js
+++ app/config/app.js
@@ -1,6 +1,6 @@
 let path = window.location.pathname
 path = path.substring(0, path.lastIndexOf('/') + 1)
-path += (process.env.NODE_ENV === 'development') ? 'app/assets/copy/api/' : 'assets/api'
+path += (process.env.NODE_ENV === 'development') ? 'app/assets/copy/api/' : 'assets/api/'
 
 export default {
   apiUrl: 'http://portal.greenbus.kz:3333/api/',
