6ae0f8954a45b5b5838962781b1c2ae729648b03
assel_s <salavatova.assel@gmail.com>
2018-07-12 00:05:32 +0600

    refactor(deadlinedTasks): display text if empty

--- app/pages/Index.vue
+++ app/pages/Index.vue
@@ -4,7 +4,8 @@
       <div class="flex-left">
         <div class="white-block">
           <p class="title">Просроченные задачи</p>
-          <div class="flex mobile-block">
+          <p class="title2" v-if="deadlined.length === 0">Просроченных задач нет</p>
+          <div class="flex mobile-block" v-if="deadlined.length > 0">
             <div class="mini-table">
               <table class="mob-none">
                   <tr class="green">
