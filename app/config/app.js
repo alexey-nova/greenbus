f45f23e3214129f6097d8cf37fc53c7c1a48d3a9
Almat Ybray <almat.ybray@gmail.com>
2018-07-29 12:52:00 +0600

    zxc

--- app/config/app.js
+++ app/config/app.js
@@ -3,19 +3,19 @@ path = path.substring(0, path.lastIndexOf('/') + 1)
 path += (process.env.NODE_ENV === 'development') ? 'app/assets/copy/api/' : 'assets/api/'
 let apiUrl, soketUrl, fileUrl
 if (process.env.NODE_ENV === 'development') {
-  apiUrl = 'http://localhost:3333/api/',
+  apiUrl = 'http://localhost:3333/api/'
   soketUrl = 'http://localhost:3333/'
   fileUrl = 'http://localhost:3333/'
 } else {
-  apiUrl = 'http://portal.greenbus.kz:3333/api/',
+  apiUrl = 'http://portal.greenbus.kz:3333/api/'
   soketUrl = 'http://portal.greenbus.kz:3333/'
   fileUrl = 'http://portal.greenbus.kz:3333/'
 }
 
 export default {
-  // apiUrl,
-  // soketUrl,
-  // fileUrl,
+  apiUrl,
+  soketUrl,
+  fileUrl,
   // apiUrl: 'http://portal.greenbus.kz:3333/api/',
   // soketUrl: 'http://portal.greenbus.kz:3333/',
   // fileUrl: 'http://portal.greenbus.kz:3333/',
@@ -27,8 +27,8 @@ export default {
   // soketUrl: 'http://195.93.152.79:3333/',
   // fileUrl: 'http://195.93.152.79:3333/',
   // вообще другой тестовый сервер
-  apiUrl: 'http://159.89.98.114:3333/api/',
-  soketUrl: 'http://159.89.98.114:3333/',
-  fileUrl: 'http://159.98.89.114:3333/',
-  mockApiUrl:  path,
+  // apiUrl: 'http://159.89.98.114:3333/api/',
+  // soketUrl: 'http://159.89.98.114:3333/',
+  // fileUrl: 'http://159.98.89.114:3333/',
+  mockApiUrl: path
 }
