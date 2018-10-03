af695d6ca0e1333777ba8d9cd348c15f1bd092b6
nurbek0298 <nurbek0298@mail.ru>
2018-10-03 17:49:04 +0600

    asddasdas

--- app/config/app.js
+++ app/config/app.js
@@ -3,13 +3,13 @@ path = path.substring(0, path.lastIndexOf('/') + 1)
 path += (process.env.NODE_ENV === 'development') ? 'app/assets/copy/api/' : 'assets/api/'
 let apiUrl, soketUrl, fileUrl
 if (process.env.NODE_ENV === 'development') {
-  apiUrl = 'http://localhost:3333/api/'
+  apiUrl = 'https://localhost:3333/api/'
   soketUrl = 'http://localhost:3333/'
-  fileUrl = 'http://localhost:3333/'
+  fileUrl = 'https://localhost:3333/'
 } else {
-  apiUrl = 'http://portal.greenbus.kz:3333/api/'
+  apiUrl = 'https://portal.greenbus.kz:3333/api/'
   soketUrl = 'http://portal.greenbus.kz:3333/'
-  fileUrl = 'http://portal.greenbus.kz:3333/'
+  fileUrl = 'https://portal.greenbus.kz:3333/'
 }
 
 export default {
