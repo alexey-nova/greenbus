049116ea3fb91504faa2c8841f861538d3dbc2a5
nurbek0298 <nurbek0298@mail.ru>
2018-10-03 12:24:37 +0600

    fixed https->http(localhost)

--- app/config/app.js
+++ app/config/app.js
@@ -3,9 +3,9 @@ path = path.substring(0, path.lastIndexOf('/') + 1)
 path += (process.env.NODE_ENV === 'development') ? 'app/assets/copy/api/' : 'assets/api/'
 let apiUrl, soketUrl, fileUrl
 if (process.env.NODE_ENV === 'development') {
-  apiUrl = 'https://localhost:3333/api/'
-  soketUrl = 'https://localhost:3333/'
-  fileUrl = 'https://localhost:3333/'
+  apiUrl = 'http://localhost:3333/api/'
+  soketUrl = 'http://localhost:3333/'
+  fileUrl = 'http://localhost:3333/'
 } else {
   apiUrl = 'https://portal.greenbus.kz:3333/api/'
   soketUrl = 'https://portal.greenbus.kz:3333/'
